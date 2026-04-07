bf5d029fcfea0bb475eaa6d25b17ca9f4a07cd43
nupurkantiya <nupurkantiya@gmail.com>
2026-04-07 18:14:51 +0530

    feat: replace HomePage with LandingExperience component in routing

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom"
-import Hero from "../components/hero/Hero"
+import LandingExperience from "../components/landing/LandingExperience"
 import Shop from "../pages/Shop"
 import ProductDetails from "../pages/ProductDetails"
 import Cart from "../pages/Cart"
@@ -7,21 +7,10 @@ import Cart from "../pages/Cart"
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<LandingExperience />} />
       <Route path="/shop" element={<Shop />} />
       <Route path="/product/:id" element={<ProductDetails />} />
       <Route path="/cart" element={<Cart />} />
     </Routes>
-  )
-}
-
-// Temporary Home page component
-function HomePage() {
-  return (
-    <>
-      <Hero />
-      {/* Spacer to enable scrolling (temporary) */}
-      <div style={{ height: "100vh", background: "#0f0f14" }} />
-    </>
   )
 }
