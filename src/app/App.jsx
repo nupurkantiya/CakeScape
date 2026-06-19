1fef7c9232cdef4f0113bc59b7ab49c2ff5ea7e1
nupurkantiya <nupurkantiya@gmail.com>
2026-06-19 16:11:47 +0530

    feat: implement cake builder context and creator hub with preset design support

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,18 +1,23 @@
 import { Routes, Route } from "react-router-dom"
+import Layout from "../components/layout/Layout"
 import LandingExperience from "../components/landing/LandingExperience"
 import Shop from "../pages/Shop"
 import ProductDetails from "../pages/ProductDetails"
 import Cart from "../pages/Cart"
 import Builder from "../pages/Builder"
+import CreatorHub from "../pages/CreatorHub"
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LandingExperience />} />
-      <Route path="/shop" element={<Shop />} />
-      <Route path="/product/:id" element={<ProductDetails />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/builder" element={<Builder />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<LandingExperience />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/builder" element={<Builder />} />
+        <Route path="/creator-hub" element={<CreatorHub />} />
+      </Routes>
+    </Layout>
   )
 }
