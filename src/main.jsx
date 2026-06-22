406d97afde916dbeaf5d1ff2bc48bb7a5bc25058
nupurkantiya <nupurkantiya@gmail.com>
2026-06-22 14:17:00 +0530

    feat: implement core e-commerce cart, product details, and global state management providers

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,18 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
 import { BuilderProvider } from "./context/BuilderContext";
+import { LocaleProvider } from "./context/LocaleContext";
 import App from "./app/App";
 import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <CartProvider>
-      <BuilderProvider>
-        <App />
-      </BuilderProvider>
-    </CartProvider>
+    <LocaleProvider>
+      <CartProvider>
+        <BuilderProvider>
+          <App />
+        </BuilderProvider>
+      </CartProvider>
+    </LocaleProvider>
   </BrowserRouter>
 );
